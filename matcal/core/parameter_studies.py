e31719d65e35dbe203a8730597b50a521c5100df
dericci <dericci@sandia.gov>
2025-08-22 12:30:01 -0600

    test KFoldCrossValidation added

--- matcal/core/parameter_studies.py
+++ matcal/core/parameter_studies.py
@@ -2157,6 +2157,7 @@ class KFoldCrossValidation:
         import matplotlib
         from joblib import Parallel, delayed
 
+        X = np.atleast_2d(X)
         nsamples = X.shape[0]
         if self.group_kfold:
             assert groups is not None
