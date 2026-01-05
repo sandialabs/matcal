28881878dcf218103eaf9680992e8764a6be72ae
Kyle N. Karlson <knkarls@sandia.gov>
2026-01-05 16:03:54 -0700

    more changes for pyapprox testing

--- matcal/core/tests/integration/test_adaptive_surrogates.py
+++ matcal/core/tests/integration/test_adaptive_surrogates.py
@@ -3,7 +3,7 @@ import numpy as np
 import unittest
 
 from matcal.core.tests.MatcalUnitTest import MatcalUnitTest
-from matcal.core.tests.unit.test_adaptive_surrogates import HAS_PYAPPROX
+from matcal.core.tests.unit.test_adaptive_surrogates import has_pyapprox
 
 
 def model(a,b,c, **kwargs):
@@ -25,7 +25,7 @@ class TestSparseGridAdaptiveSurrogate(MatcalUnitTest):
     def setUp(self):
         super().setUp(__file__)           
 
-    @unittest.skipIf(not HAS_PYAPPROX,
+    @unittest.skipIf(not has_pyapprox(),
                  "pyapprox not installed – skipping pyapprox‑dependent tests")
     def test_simple_function_fit(self):
         sg_study = mc.SparseGridAdaptiveSurrogateStudy(a,b,c)
